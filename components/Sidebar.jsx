b1ab4b77cda56fabbfe825943d4efd549d8e7ef7
Koushik Guria <koushikguria998@gmail.com>
2025-06-09 18:15:46 +0530

    Prompt box added

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -47,6 +47,12 @@ const Sidebar = ({expand, setExpand}) => {
           </div>
           {expand && <> <span>Get App</span> <Image src={assets.new_icon} alt='' /> </>}
         </div>
+
+        <div className={`flex items-center ${expand ? 'hover:bg-white/10 rounded-lg' : 'justify-center w-full'} gap-3 text-white/60 text-sm p-2 mt-2 cursor-pointer`}>
+          <Image className='w-7' src={assets.profile_icon} alt='' />
+          {expand && <span>My Profile</span>}
+        </div>
+
       </div>
     </div>
   )
