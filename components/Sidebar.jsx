a580abed721f03fb002b368b56269937dcdabc0f
Koushik Guria <koushikguria998@gmail.com>
2025-06-10 07:22:38 +0530

    Frontend part updated

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { assets } from '@/assets/assets';
 import { useClerk, UserButton } from '@clerk/nextjs';
@@ -9,6 +9,7 @@ const Sidebar = ({expand, setExpand}) => {
 
   const { openSignIn } = useClerk();
   const { user } = useAppContext();
+  const [openMenu, setOpenMenu] = useState({id: 0, open: false});
 
   return (
     <div className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
@@ -39,7 +40,7 @@ const Sidebar = ({expand, setExpand}) => {
         <div className={`mt-8 text-white/25 text-sm ${expand ? 'block' : 'hidden'}`}>
           <p className='my-1'>Recents</p>
 
-          <ChatLabel/>
+          <ChatLabel openMenu={openMenu} setOpenMenu={setOpenMenu}/>
           
         </div>
       </div>
